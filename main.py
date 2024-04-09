43982a6b7bee52fb83deb0995589ba6a41ed3dce
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-04-09 11:30:19 +0530

    improved

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
 import hashlib
+import datetime
 
-def hashGenerator(data):
+def hash_generator(data):
     sha = hashlib.sha256(data.encode())
     return sha.hexdigest()
 
 class Block:
-
-    def __init__(self, data, hash, prev_hash):
+    def __init__(self, data, timestamp, prev_hash):
         self.data = data
-        self.hash = hash
+        self.timestamp = timestamp
         self.prev_hash = prev_hash
-        
+        self.hash = self.generate_hash()
 
-class BlockChain:
-    
-        def __init__(self):
-             hashLastBlock = hashGenerator("Genesis Block")
-             hashFirstBlock = hashGenerator("First Block")
+    def generate_hash(self):
+        data = str(self.data) + str(self.timestamp) + str(self.prev_hash)
+        return hash_generator(data)
 
-             genesisBlock = Block("Genesis Block", hashFirstBlock, hashLastBlock)
-             self.chain = [genesisBlock]
-    
-        def addBlock(self, data):
-            prev_hash = self.chain[-1].hash
-            hash = hashGenerator(data)
-            newBlock = Block(data, hash, prev_hash)
-            self.chain.append(newBlock)
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
 
-myChain = BlockChain()
+    def create_genesis_block(self):
+        return Block("Genesis Block", datetime.datetime.now(), "0")
 
-myChain.addBlock("Second Block")
-myChain.addBlock("Third Block")
-myChain.addBlock("Fourth Block")
+    def add_block(self, data):
+        prev_hash = self.chain[-1].hash
+        timestamp = datetime.datetime.now()
+        new_block = Block(data, timestamp, prev_hash)
+        self.chain.append(new_block)
 
-for block in myChain.chain:
-     print(block.__dict__)
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+
+            if current_block.hash != current_block.generate_hash():
+                return False
+
+            if current_block.prev_hash != previous_block.hash:
+                return False
+
+        return True
+
+if __name__ == "__main__":
+    my_chain = Blockchain()
+
+    my_chain.add_block("Second Block")
+    my_chain.add_block("Third Block")
+    my_chain.add_block("Fourth Block")
+
+    for block in my_chain.chain:
+        print(block.__dict__)
+
+    print("Is the blockchain valid?", my_chain.is_valid())
